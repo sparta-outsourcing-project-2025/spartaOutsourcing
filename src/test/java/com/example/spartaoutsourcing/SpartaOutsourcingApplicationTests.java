efe80b4287189df6143cacec78f971dc4f92a884
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-02 20:01:02 +0900

    first commit

---
+++ src/test/java/com/example/spartaoutsourcing/SpartaOutsourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartaoutsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaOutsourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
