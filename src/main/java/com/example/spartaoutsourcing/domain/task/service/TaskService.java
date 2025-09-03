e4d5c45ac25f17307a90a4b184f5876783caa034
lby9906 <84373979+lby9906@users.noreply.github.com>
2025-09-03 21:01:00 +0900

    feat: task 상세 조회 기능 구현 (#13)

--- src/main/java/com/example/spartaoutsourcing/domain/task/service/TaskService.java
+++ src/main/java/com/example/spartaoutsourcing/domain/task/service/TaskService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.spartaoutsourcing.common.consts.ErrorCode;
+import com.example.spartaoutsourcing.common.dto.AuthUserRequest;
 import com.example.spartaoutsourcing.common.exception.GlobalException;
 import com.example.spartaoutsourcing.domain.task.dto.request.TaskRequest;
 import com.example.spartaoutsourcing.domain.task.dto.response.Assignee;
@@ -12,7 +13,6 @@ import com.example.spartaoutsourcing.domain.task.entity.Task;
 import com.example.spartaoutsourcing.domain.task.enums.TaskStatus;
 import com.example.spartaoutsourcing.domain.task.repository.TaskRepository;
 import com.example.spartaoutsourcing.domain.user.entity.User;
-import com.example.spartaoutsourcing.domain.user.repository.UserRepository;
 import com.example.spartaoutsourcing.domain.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -30,13 +30,32 @@ public class TaskService {
 	 * @RequestBody Task 요청 정보
 	 * @return 공통 응답과 Task 생성 응답 반환
 	*/
-	public TaskResponse save(TaskRequest taskRequest) {
-		User user = userService.getUserById(taskRequest.getAssigneeId());
+	public TaskResponse save(AuthUserRequest authUserRequest, TaskRequest taskRequest) {
+		User user = userService.getUserById(authUserRequest.getId());
 
 		Task task = Task.of(taskRequest.getTitle(), taskRequest.getDescription(), TaskStatus.TODO,
 			taskRequest.getDueDate(), taskRequest.getPriority(), user);
 
 		taskRepository.save(task);
+
+		Assignee assignee = Assignee.of(user.getId(), user.getUsername(), user.getName(), user.getEmail());
+
+		return TaskResponse.of(task.getId(), task.getTitle(), task.getDescription(), task.getDueDate(),
+			task.getTaskPriority(), task.getTaskStatus(), user.getId(), assignee, task.getCreatedAt(), task.getModifiedAt());
+	}
+
+	/**
+	 * 특정 사용자 Task 상세 조회
+	 * @PathVariable Task Id 요청
+	 * @return 공통 응답과 Task 상세 조회 응답 반환
+	 * */
+
+	@Transactional(readOnly = true)
+	public TaskResponse getTask(AuthUserRequest authUserRequest, Long taskId) {
+		User user = userService.getUserById(authUserRequest.getId());
+
+		Task task = taskRepository.findByIdAndUserId(taskId, user.getId()).orElseThrow(() ->
+			new GlobalException(ErrorCode.TASK_NOT_FOUND));
 
 		Assignee assignee = Assignee.of(user.getId(), user.getUsername(), user.getName(), user.getEmail());
 
