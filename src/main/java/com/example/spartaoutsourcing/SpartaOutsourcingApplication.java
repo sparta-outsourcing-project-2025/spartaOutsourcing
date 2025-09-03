deee6b681e439dc8a1f3d662002981f595ed940d
kisu kim <qpsrltn@gmail.com>
2025-09-03 15:35:58 +0900

    fix: @EnableJpaAuditing 추가

--- src/main/java/com/example/spartaoutsourcing/SpartaOutsourcingApplication.java
+++ src/main/java/com/example/spartaoutsourcing/SpartaOutsourcingApplication.java
@@ -2,8 +2,10 @@ package com.example.spartaoutsourcing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpartaOutsourcingApplication {
 
 	public static void main(String[] args) {
