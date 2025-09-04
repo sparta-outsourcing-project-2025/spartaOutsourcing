9c1203ff8d4310e31e1eb420332e1813fac74d75
lby9906 <84373979+lby9906@users.noreply.github.com>
2025-09-04 11:16:15 +0900

    feat: task 수정 기능 구현 (#25)

--- src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
+++ src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
@@ -11,7 +11,8 @@ public enum SuccessCode {
     OK(200, "요청 성공"),
     SUCCESS_GET_USER(200, "사용자 정보를 조회했습니다."),
     TASK_CREATED(201, "Task가 생성되었습니다."),
-    TASK_FIND(200, "Task를 조회했습니다.");
+    TASK_FIND(200, "Task를 조회했습니다."),
+    TASK_UPDATED(200, "Task가 수정되었습니다.");
 
     private final int status;
     private final String message;
