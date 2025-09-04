ab1c1bc21b9abda744e4acc07235494296491d7c
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-04 14:25:45 +0900

    Feture/3 comment create (#29)
    * feat: Comment 생성 기능 구현
    
    * feat: Comment 생성 기능 구현

--- src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
+++ src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
@@ -7,19 +7,31 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SuccessCode {
-    CREATED(200, "회원가입이 완료되었습니다."),
+
+    // COMMON
     OK(200, "요청 성공"),
+
+    // USER
+    CREATED(200, "회원가입이 완료되었습니다."),
     SUCCESS_GET_USER(200, "사용자 정보를 조회했습니다."),
-    TASK_CREATED(201, "Task가 생성되었습니다."),
-
-    TEAM_CREATED(201, "팀이 성공적으로 생성되었습니다."),
-    SUCCESS_GET_TEAM(200,"팀 목록을 조회했습니다."),
-
-
-    TASK_FIND(200, "Task를 조회했습니다."),
     SUCCESS_LOGIN(200, "로그인이 완료되었습니다."),
     SUCCESS_DELETE_USER(200, "회원탈퇴가 완료되었습니다."),
-    TASK_UPDATED(200, "Task가 수정되었습니다.");
+
+    // TASK
+    TASK_FIND(200, "Task를 조회했습니다."),
+    TASK_UPDATED(200, "Task가 수정되었습니다."),
+    TASK_CREATED(201, "Task가 생성되었습니다."),
+
+    // TEAM
+    TEAM_CREATED(201, "팀 정보가 성공적으로 업데이트되었습니다."),
+    SUCCESS_GET_TEAM(200,"팀 목록을 조회했습니다."),
+    TEAM_CREATED(201, "팀이 성공적으로 생성되었습니다."),
+
+    // COMMENT
+    COMMENT_FIND(200,"댓글을 조회했습니다."),
+    COMMENT_UPDATED(200, "댓글이 수정되었습니다."),
+    COMMENT_CREATED(201, "댓글이 생성되었습니다.");
+
 
     private final int status;
     private final String message;
