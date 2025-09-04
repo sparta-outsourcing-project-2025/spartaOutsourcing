56a125a5e65dc4c15c0b8d00dbbe37965fd2b4e1
doldollee00 <50581232+doldollee00@users.noreply.github.com>
2025-09-04 11:18:07 +0900

    feat: auth 회원가입, 로그인 기능 구현 완료 (#16)
    * feat: 회원가입 및 로그인 구현

--- src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
+++ src/main/java/com/example/spartaoutsourcing/common/consts/SuccessCode.java
@@ -12,6 +12,8 @@ public enum SuccessCode {
     SUCCESS_GET_USER(200, "사용자 정보를 조회했습니다."),
     TASK_CREATED(201, "Task가 생성되었습니다."),
     TASK_FIND(200, "Task를 조회했습니다."),
+    SUCCESS_LOGIN(200, "로그인이 완료되었습니다."),
+    SUCCESS_DELETE_USER(200, "회원탈퇴가 완료되었습니다."),
     TASK_UPDATED(200, "Task가 수정되었습니다.");
 
     private final int status;
