ba1ff0c5d7d6f1b054d5d7558b898f1cbaada11d
doldollee00 <doldollee00@gmail.com>
2025-09-05 12:18:52 +0900

    refactor: soft delete (@where -> @SQLRestriction 변경)

--- src/main/java/com/example/spartaoutsourcing/domain/user/entity/User.java
+++ src/main/java/com/example/spartaoutsourcing/domain/user/entity/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name="users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
 public class User extends AuditableEntity {
     @Column(nullable = false, length = 20)
     private String username;
