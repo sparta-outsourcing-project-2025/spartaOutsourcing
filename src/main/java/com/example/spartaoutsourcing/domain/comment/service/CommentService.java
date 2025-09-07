01da5f5eda3dda784afc152b3a09670c46599160
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-09-07 19:25:05 +0900

    feat: Comment 수정 및 삭제 기능 구현

--- src/main/java/com/example/spartaoutsourcing/domain/comment/service/CommentService.java
+++ src/main/java/com/example/spartaoutsourcing/domain/comment/service/CommentService.java
@@ -5,14 +5,15 @@ import com.example.spartaoutsourcing.common.dto.AuthUserRequest;
 import com.example.spartaoutsourcing.common.dto.PageResponseDto;
 import com.example.spartaoutsourcing.common.exception.GlobalException;
 import com.example.spartaoutsourcing.domain.comment.dto.request.CommentSaveRequest;
+import com.example.spartaoutsourcing.domain.comment.dto.request.CommentUpdateRequest;
 import com.example.spartaoutsourcing.domain.comment.dto.response.CommentResponse;
 import com.example.spartaoutsourcing.domain.comment.entity.Comment;
 import com.example.spartaoutsourcing.domain.comment.repository.CommentRepository;
 import com.example.spartaoutsourcing.domain.task.entity.Task;
-import com.example.spartaoutsourcing.domain.task.repository.TaskRepository;
+import com.example.spartaoutsourcing.domain.task.service.TaskService;
 import com.example.spartaoutsourcing.domain.user.dto.UserCommentResponse;
 import com.example.spartaoutsourcing.domain.user.entity.User;
-import com.example.spartaoutsourcing.domain.user.repository.UserRepository;
+import com.example.spartaoutsourcing.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,21 +27,17 @@ import java.util.List;
 public class CommentService {
 
     private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
-    private final TaskRepository taskRepository;
+    private final UserService userService;
+    private final TaskService taskService;
 
     @Transactional
     public CommentResponse save(AuthUserRequest authUserRequest, long taskId, CommentSaveRequest request) {
-        User user = userRepository.findById(authUserRequest.getId()).orElseThrow(
-                () -> new GlobalException(ErrorCode.USER_NOT_FOUND)
-        );
-        Task task = taskRepository.findById(taskId).orElseThrow(
-                () -> new GlobalException(ErrorCode.TASK_NOT_FOUND)
-        );
+        User user = userService.getUserById(authUserRequest.getId());
+        Task task = taskService.getTesKById(taskId);
         Comment parentComment = null;
         if (request.getParentId() != null) {
             parentComment = commentRepository.findById(request.getParentId()).orElseThrow(
-                    () -> new GlobalException(ErrorCode.NOT_FOUND)
+                    () -> new GlobalException(ErrorCode.COMMENT_NOT_FOUND)
             );
         }
 
@@ -60,9 +57,7 @@ public class CommentService {
     }
 
     public PageResponseDto<CommentResponse> getComments(Long taskId, Long page, Long size, String sort) {
-        taskRepository.findById(taskId).orElseThrow(
-                () -> new GlobalException(ErrorCode.TASK_NOT_FOUND)
-        );
+        taskService.getTesKById(taskId);
         long offset = (page-1) * size;
         List<Comment> rootComments = sort.equalsIgnoreCase("oldest")
                 ? commentRepository.findAllByTaskIdOrderByAsc(taskId, size, offset) : commentRepository.findAllByTaskIdOrderByDesc(taskId, size, offset);
@@ -107,5 +102,49 @@ public class CommentService {
         Long totalElements = commentRepository.countAllByTaskId(taskId);
         int totalPage= (int)Math.ceil((double)totalElements / size);
         return PageResponseDto.of(comments, totalElements, totalPage, size, page);
+    }
+
+    public CommentResponse updateComment(AuthUserRequest authUserRequest, Long commentId, Long taskId, CommentUpdateRequest request) {
+        Task task = taskService.getTesKById(taskId);
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new GlobalException(ErrorCode.COMMENT_NOT_FOUND)
+        );
+        if (!comment.getUser().getId().equals(authUserRequest.getId())) {
+            throw new GlobalException(ErrorCode.FORBIDDEN);
+        }
+        comment.update(request.getContent());
+
+        return CommentResponse.of(
+                comment.getId(),
+                comment.getContent(),
+                task.getId(),
+                UserCommentResponse.of(
+                        comment.getUser().getId(),
+                        comment.getUser().getUsername(),
+                        comment.getUser().getName(),
+                        comment.getUser().getEmail(),
+                        comment.getUser().getRole().toString()
+                ),
+                comment.getParent().getId(),
+                comment.getCreatedAt(),
+                comment.getModifiedAt()
+        );
+    }
+
+
+    public void deleteComment(AuthUserRequest authUserRequest, Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow( () -> new GlobalException(ErrorCode.COMMENT_NOT_FOUND));
+        if (!comment.getUser().getId().equals(authUserRequest.getId())) {
+            throw new GlobalException(ErrorCode.FORBIDDEN);
+        }
+        if (comment.getParent() != null) {
+            comment.delete();
+            return;
+        }
+        comment.delete();
+        List<Comment> comments = commentRepository.findAllByParentId(comment.getId());
+        for (Comment c : comments) {
+            c.delete();
+        }
     }
 }
