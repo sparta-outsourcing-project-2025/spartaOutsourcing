60f711f393a875ce2f4ff2aadc048acea3faf315
lby9906 <84373979+lby9906@users.noreply.github.com>
2025-09-08 16:25:56 +0900

    feat: taskService 테스트 코드 작성 (#102)

--- src/main/java/com/example/spartaoutsourcing/domain/task/dto/request/TaskRequest.java
+++ src/main/java/com/example/spartaoutsourcing/domain/task/dto/request/TaskRequest.java
@@ -6,9 +6,11 @@ import com.example.spartaoutsourcing.domain.task.enums.TaskPriority;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class TaskRequest {
 
 	@NotBlank(message = "제목은 필수입니다.")
